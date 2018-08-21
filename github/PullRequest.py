d2ff2a8307b7471a894d6c8d1761fceae1b7cd67
Masashi SAKAI <masashi.sakai1986@gmail.com>
2018-08-22 07:24:15 +0900

    Add the "since" argument to work the same way as Issue.get_comments()

--- github/PullRequest.py
+++ github/PullRequest.py
@@ -611,16 +611,21 @@ class PullRequest(github.GithubObject.CompletableGithubObject):
         )
         return github.IssueComment.IssueComment(self._requester, headers, data, completed=True)
 
-    def get_issue_comments(self):
+    def get_issue_comments(self, since=github.GithubObject.NotSet):
         """
         :calls: `GET /repos/:owner/:repo/issues/:number/comments <http://developer.github.com/v3/issues/comments>`_
+        :param since: datetime.datetime format YYYY-MM-DDTHH:MM:SSZ
         :rtype: :class:`github.PaginatedList.PaginatedList` of :class:`github.IssueComment.IssueComment`
         """
+        assert since is github.GithubObject.NotSet or isinstance(since, datetime.datetime), since
+        url_parameters = dict()
+        if since is not github.GithubObject.NotSet:
+            url_parameters["since"] = since.strftime("%Y-%m-%dT%H:%M:%SZ")
         return github.PaginatedList.PaginatedList(
             github.IssueComment.IssueComment,
             self._requester,
             self.issue_url + "/comments",
-            None
+            url_parameters
         )
 
     def get_review(self, id):
